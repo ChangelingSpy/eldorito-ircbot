4b4843174ee24bbdbdc7809d20c3997babff5797
Jordan Banasik <woovie@woovie.net>
2015-03-30 23:11:44 -0500

    Fixed syntax errors, commented out YT URL parse

--- doritoBot.py
+++ doritoBot.py
@@ -1,10 +1,13 @@
+#Anything commented out is WIP, most likely my URL parsing crap for youtube.
+
 import hexchat
-from urllib.request import urlopen
-from urllib.error import URLError
 import json
 import re
 import string
 from time import sleep
+#from urllib.request import urlopen
+#from urllib.error import URLError
+
 
 __module_name__ = "doritoBot"
 __module_version__ = "0.1"
@@ -17,8 +20,8 @@ private_channel = "#dorito-dev"
 pub = hexchat.find_context(server=eldorito_server, channel=public_channel)
 dev = hexchat.find_context(server=eldorito_server, channel=private_channel)
 
-apiKey = 'AIzaSyAVcm67qjgMd3m9tvF_vvctD-k0Es8TwtA'
-partOptions = 'snippet,contentDetails,statistics'
+#apiKey = 'AIzaSyAVcm67qjgMd3m9tvF_vvctD-k0Es8TwtA'
+#partOptions = 'snippet,contentDetails,statistics'
 
 def nameFilter (word):
     ind1 = word.find(":")
@@ -26,30 +29,30 @@ def nameFilter (word):
     return word[ind1+1:ind2]
 
 def pubChathook (word, word_eol, attr):
-    if word[2] == public_channel
-        hexchat.EAT_NONE
+    if word[2] == public_channel:
+        return hexchat.EAT_NONE
 
 def devChathook (word, word_eol, attr):
-    if word[2] == private_channel
-        hexchat.EAT_NONE
+    if word[2] == private_channel:
+        return hexchat.EAT_NONE
 
 def pubJoinhook (word, word_eol, attr):
-    if word[2] == public_channel
-        list hexchat.get_list("users")
+    if word[2] == public_channel:
+        list = hexchat.get_list("users")
         nick_filtered = nameFilter(word[0])
         power = ''
         for x in list:
-            if x.nick == nick_filtered
+            if x.nick == nick_filtered:
                 power = x.prefix
         if power == '':
-            pub.command("NOTICE "+format(nick_filtered)+":Welcome to #eldorito! Please read the topic for the latest announcements and rules!"
-    hexchat.EAT_NONE
+            pub.command("NOTICE "+format(nick_filtered)+" :Welcome to #eldorito! Please read the topic for the latest announcements and rules!")
+            return hexchat.EAT_NONE
 
 def devJoinhook (word, word_eol, attr):
-    if word[2] == private_channel
-        hexchat.EAT_NONE
+    if word[2] == private_channel:
+        return hexchat.EAT_NONE
 
 hexchat.hook_server("PRIVMSG", pubChathook)
 hexchat.hook_server("PRIVMSG", devChathook)
-hexchat.hook_server("PRIVMSG", pubJoinhook)
-hexchat.hook_server("PRIVMSG", devJoinhook)
+hexchat.hook_server("JOIN", pubJoinhook)
+hexchat.hook_server("JOIN", devJoinhook)
